7b83f57946a3f540c0301868050da30e23fcc920
unknown <manami.artmake@gmail.com>
2026-09-04 21:24:14 +0900

    cors test

--- access.js
+++ access.js
@@ -1,26 +1,3 @@
 const RESERVATION_LIFF_URL =
     "https://liff.line.me/2010613933-uBqu1yDz";
 
-const closeButton = document.getElementById("closeButton");
-
-if (closeButton) {
-    closeButton.addEventListener("click", () => {
-
-        const fromMenu =
-            sessionStorage.getItem("fromReservationMenu");
-
-        if (fromMenu === "1") {
-
-            sessionStorage.removeItem("fromReservationMenu");
-
-            window.location.href =
-                RESERVATION_LIFF_URL + "?openMenu=1";
-
-        } else {
-
-            window.close();
-
-        }
-
-    });
-}
